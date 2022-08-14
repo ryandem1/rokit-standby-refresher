9e1dfda7f6aae84e579e11ee407636e964d84e23
ryandemarigny <ryandemarigny@gmail.com>
2022-08-14 14:19:55 -0700

    Initial Commit

---
+++ rokit-standby-refresher/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  rokit-standby-refresher
+//
+//  Created by Ryan de Marigny on 8/14/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ rokit-standby-refresher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  rokit-standby-refresher
+//
+//  Created by Ryan de Marigny on 8/14/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
