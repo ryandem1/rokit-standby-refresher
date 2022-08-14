4e8a9975e65655d9737df7657cba6b932bfb1b1f
ryandemarigny <ryandemarigny@gmail.com>
2022-08-14 14:43:32 -0700

    removed the window

--- rokit-standby-refresher/AppDelegate.swift
+++ rokit-standby-refresher/AppDelegate.swift
@@ -14,19 +14,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 270),
-            styleMask: [.miniaturizable, .closable, .resizable, .titled],
-            backing: .buffered, defer: false
-        )
-        window.center()
-        window.title = "No Storyboard Window"
-        window.makeKeyAndOrderFront(nil)
-        
-        // 2
+
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        // 3
+
         if let button = statusItem.button {
             button.image = NSImage(systemSymbolName: "hifispeaker.fill", accessibilityDescription: "1")
         }
